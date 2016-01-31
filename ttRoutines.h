8379403eb74dc54c846f71f0cabc6b8ff9861737
dalek7 <dalek@kaist.ac.kr>
2016-01-31 10:14:33 +0900

    Initial commit of utils

---
+++ ttRoutines.h
@@ -0,0 +1,41 @@
+
+#ifndef TT_ROUTINES_H
+#define TT_ROUTINES_H
+
+#ifdef __cplusplus
+
+// category based on http://docs.scipy.org/doc/numpy-1.10.0/reference/routines.html
+
+template <class T>
+inline void ShuffleArray(T* src, int sz)
+{
+	for(int i=0; i<sz; i++)
+	{
+		int nPos    =  i + (rand() % (sz-i));    //Random remaining position
+		T temp		= src[i]; 
+		src[i]      = src[nPos]; 
+		src[nPos]   = temp;
+	}
+}
+
+
+template <class T>
+inline void ShiftWithNew(T* arr, const T &n_val, int szarr)
+{
+	int i;
+	for(i=0; i< sz-1; i++)
+	{
+		arr[sz-1-i] = arr[sz-2-i];
+		
+	}
+	arr[0] = n_val;
+
+}
+
+
+
+
+
+#endif /* __cplusplus */
+
+#endif
