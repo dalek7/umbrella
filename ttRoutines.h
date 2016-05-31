3e086b58472ca27704cf3ebfef651670fb266f5a
dalek7 <dalek@kaist.ac.kr>
2016-05-31 22:20:30 +0900

    added namespace

--- ttRoutines.h
+++ ttRoutines.h
@@ -1,3 +1,6 @@
+////////////////////////////////////////////////////////
+// Seung-Chan Kim
+// Since 2011
 
 #ifndef TT_ROUTINES_H
 #define TT_ROUTINES_H
@@ -5,35 +8,37 @@
 #ifdef __cplusplus
 
 // category based on http://docs.scipy.org/doc/numpy-1.10.0/reference/routines.html
+namespace DD {
 
-template <class T>
-inline void ShuffleArray(T* src, int sz)
-{
-	for(int i=0; i<sz; i++)
+	template <typename T>
+	inline void ShuffleArray(T* src, int sz)
 	{
-		int nPos    =  i + (rand() % (sz-i));    //Random remaining position
-		T temp		= src[i]; 
-		src[i]      = src[nPos]; 
-		src[nPos]   = temp;
+		for(int i=0; i<sz; i++)
+		{
+			int nPos    =  i + (rand() % (sz-i));    //Random remaining position
+			T temp		= src[i]; 
+			src[i]      = src[nPos]; 
+			src[nPos]   = temp;
+		}
 	}
-}
 
 
-template <class T>
-inline void ShiftWithNew(T* arr, const T &n_val, int sz)
-{
-	int i;
-	for(i=0; i< sz-1; i++)
+	template <typename T>
+	inline void ShiftWithNew(T* arr, const T &n_val, int szarr)
 	{
-		arr[sz-1-i] = arr[sz-2-i];
-		
+		int i;
+		for(i=0; i< sz-1; i++)
+		{
+			arr[sz-1-i] = arr[sz-2-i];
+			
+		}
+		arr[0] = n_val;
+
 	}
-	arr[0] = n_val;
+
+
 
 }
-
-
-
 
 
 #endif /* __cplusplus */
