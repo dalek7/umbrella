b497c1c501cd3cae104986dbbcb198876f64a426
dalek7 <dalek@kaist.ac.kr>
2017-01-01 10:14:36 +0900

    Added a usage

--- Matlab/GetSparseIDX.m
+++ Matlab/GetSparseIDX.m
@@ -1,6 +1,7 @@
 function idxsparse = GetSparseIDX(data_in, nStride)
 % Written by Seung-Chan Kim @ Disney Research, Pittsburgh, 2011
 % Example:
+% idx = 1:size(A, 1);
 % idsparse = GetSparseIDX(idx, 4);
 % A = A(idxsparse, :);
     
