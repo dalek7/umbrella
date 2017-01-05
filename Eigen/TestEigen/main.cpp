207db32be29f50519b5ef25e6a34e409c584d9b6
dalek7 <dalek@kaist.ac.kr>
2017-01-05 22:56:35 +0900

    added Eigen::Isometry3d

--- Eigen/TestEigen/main.cpp
+++ Eigen/TestEigen/main.cpp
@@ -8,13 +8,45 @@ using Eigen::MatrixXd;
 using namespace std;
 
 void TestEigenMatrix1();
+void TestEigenMatrix2();
+
 int main()
 {
-    TestEigenMatrix1();
+    TestEigenMatrix2();
     cout << "Finished!" << endl;
     return 0;
 }
 
+void TestEigenMatrix2()
+{
+    int nodesPerLevel = 36;
+    char buf[255]={0,};
+    for (int n = 0; n < nodesPerLevel; n++)
+    {
+        Eigen::AngleAxisd rotz(-M_PI + 2*n*M_PI / nodesPerLevel, Eigen::Vector3d::UnitZ());
+        //Eigen::AngleAxisd roty(-0.5*M_PI + id*M_PI / (numLaps * nodesPerLevel), Eigen::Vector3d::UnitY());
+        Eigen::Matrix3d rot = rotz.toRotationMatrix();// (rotz * roty).toRotationMatrix();
+        Eigen::Isometry3d t;
+        t = rot;
+        
+        cout << "#" << n << endl;
+        //row-wise
+        sprintf(buf, "%f\t%f\t%f\t%f\n%f\t%f\t%f\t%f\n%f\t%f\t%f\t%f\n%f\t%f\t%f\t%f",
+                t(0,0), t(0,1),t(0,2), t(0,3),
+                t(1,0), t(1,1),t(1,2), t(1,3),
+                t(2,0), t(2,1),t(2,2), t(2,3),
+                t(3,0), t(3,1),t(3,2), t(3,3) );
+        cout << buf << endl<< endl;
+        
+        sprintf(buf, "%f\t%f\t%f\n%f\t%f\t%f\n%f\t%f\t%f",
+                rot(0,0), rot(0,1),rot(0,2),
+                rot(1,0), rot(1,1),rot(1,2),
+                rot(2,0), rot(2,1),rot(2,2));
+        cout << buf << endl<< endl;
+
+    }
+    
+}
 
 // from https://eigen.tuxfamily.org/dox-devel/group__TutorialAdvancedInitialization.html
 void TestEigenMatrix1()
