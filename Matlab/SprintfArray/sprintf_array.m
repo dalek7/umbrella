3ff2769e0f6174ba34e25771e658ae4079b685d6
seung <ncraig@naver.com>
2018-08-15 11:39:14 +0900

    sprintf array

---
+++ Matlab/SprintfArray/sprintf_array.m
@@ -0,0 +1,5 @@
+function str = sprintf_array(vec)
+% based on https://www.mathworks.com/matlabcentral/answers/341189-how-to-automatically-sprintf-an-array-elements#answer_267802
+str = sprintf('%d,',vec);
+
+end
---
+++ Matlab/SprintfArray/sprintftest.m
@@ -0,0 +1,4 @@
+clear; close all; clc;
+A = [1 2 3];
+Astr = sprintf_array(A);
+disp(Astr);
